1575274b663a314c23587cb9f51489ec2d71cfc9
GitGhillie <jojo_jillis@hotmail.com>
2017-06-04 23:37:16 +0200

    Added main program loop with xbee interface. Temporarily using older version of initializing maze until the bugs are ironed out.

--- LijnVolger/main.c
+++ LijnVolger/main.c
@@ -1,26 +1,55 @@
 #include <string.h>
+#include <stdio.h>
+#include <Windows.h>
 
 #include "typedefs.h"
 #include "maze.h"
 #include "router.h"
+#include "xbee.h"
+
 
 coords getCoords(char name[]);
 cell maze[13][13];
+int programStatus = 1; //0 = program should turn of, 1 = should run.
+
 
 int main()
 {
+    char byteBuffer[BUFSIZ+1];
     //Initialization of maze.
     initMinOnes();
     initZeros();
     nameMaze("MazeNames.txt"); //Filename with description of maze.
-
-
-    //Post initialization events here.
     blockEdges();   //Find out what edges/crossings need to be blocked.
     router();       //Lee's algorithm.
 
-    displayMaze();
+    //Xbee initialization.
+    HANDLE hSerial = NULL;
 
+    initXbee(hSerial);
+
+    //Main loop of the program.
+    while(programStatus)
+    {
+    readByte(hSerial, byteBuffer);
+
+    //////////////////////////////
+    //    Decide what do to based on byteBuffer here, and write to it.
+
+    gets(byteBuffer); //Temporary user input.
+    //////////////////////////////
+
+    writeByte(hSerial, byteBuffer);
+
+    if (byteBuffer[0] == 'q') // end the loop by typing 'q'
+        programStatus = 0;
+
+
+    displayMaze();
+    }
+
+
+    CloseHandle(hSerial); //Close serial handle (important).
     return 0;
 }
 
---
+++ LijnVolger/xbee.h
@@ -0,0 +1,12 @@
+#ifndef XBEE_H_INCLUDED
+#define XBEE_H_INCLUDED
+
+#include <Windows.h>
+
+void initXbee(HANDLE hSerial);
+
+void initSio(HANDLE hSerial);
+int readByte(HANDLE hSerial, char *buffRead);
+int writeByte(HANDLE hSerial, char *buffWrite);
+
+#endif // XBEE_H_INCLUDED
