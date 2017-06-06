45d304452fc2b41b24cee28c3cbe7e3206596447
Evochrome <jetse.spijkstra@gmail.com>
2017-06-06 17:09:51 +0200

    Find_nearest_point works

--- LijnVolger/router.h
+++ LijnVolger/router.h
@@ -13,6 +13,7 @@ coords getCoords(char name[]); //Defined in main.c
 void router();
 void spread(coords endPos, coords startPos);
 void traceBack(coords endPos, coords startPos);
+int find_nearest_point(char* point1, char* point2, char* point3);
 
 void setSurroundings(int x, int y, int i);
 coords checkSurroundings(coords curPos);
