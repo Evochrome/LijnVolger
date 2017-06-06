c87acf04236ef7476c079c42929efc2fde3a370e
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2017-06-06 16:49:54 +0200

    Basic structure of decision tree implemented
    Added a switch which will create a signal_out, based on signal_in and the elapsed time. Mostly comments now, as more functions need to be implemented

--- LijnVolger/cmake-build-debug/cmake_install.cmake
+++ LijnVolger/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jetse/Documents/GitHub/MazeRouter/LijnVolger/LijnVolger
+# Install script for directory: C:/Users/Stefanie/CLionProjects/LineFollower/LijnVolger/LijnVolger
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jetse/Documents/GitHub/MazeRouter/LijnVolger/LijnVolger/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Stefanie/CLionProjects/LineFollower/LijnVolger/LijnVolger/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- LijnVolger/main.c
+++ LijnVolger/main.c
@@ -9,6 +9,7 @@ void init_time();
 double get_time();
 cell maze[13][13];
 clock_t t_start;
+double t_line = 10.0; //Amount of seconds it takes to travel between two crossings
 
 int main()
 {
@@ -59,4 +60,37 @@ void init_time ()
 double get_time()
 {
     return ((double) (clock() - t_start) / CLOCKS_PER_SEC);
+}
+
+
+int signal_in;
+int signal_out;
+void decide_instruction(signal_in)
+{
+    double t_elapsed = get_time();
+    switch(signal_in)
+    {
+        case 9: //"00001001" -> mine
+            if((t_elapsed > 0.25*t_line) && (t_elapsed < 0.75*t_line))
+            {
+                //mine:Recheck route
+                //Take a turn around left (00001111=15) or right (00001010=10)
+            }
+            //Go to next place in route
+            break;
+        case 6: //"00000110" -> crossing, corner state, endpoint
+            if((t_elapsed > 0.75*t_line) && (t_elapsed < 1.25*t_line))
+            {
+                //  Crossing: left(6), right(3) or straight ahead (0), for the corner straight does not exist
+            }
+            else if ((t_elapsed > 0.15*t_line) && (t_elapsed < 0.65*t_line))
+            {
+                //endpoint:Take a turn around left (00001111=15) or right (00001010=10)
+            }
+            //Go to next place in route
+            break;
+        case 10: //"00001010" ->Receiving error
+            break; //Signal_out is not changed
+        default: signal_out = 0; break;
+    }
 }
