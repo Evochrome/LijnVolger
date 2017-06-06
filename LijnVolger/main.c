d276b0322c0db14bc8e66b89d512dfab0d58ce33
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2017-06-06 17:06:18 +0200

    Minor changes
    Added default straight line if timing is not correct

--- LijnVolger/main.c
+++ LijnVolger/main.c
@@ -75,19 +75,22 @@ void decide_instruction(signal_in)
             {
                 //mine:Recheck route
                 //Take a turn around left (00001111=15) or right (00001010=10)
+                //Go to next place in route
             }
-            //Go to next place in route
+            else signal_out = 0;
             break;
         case 6: //"00000110" -> crossing, corner state, endpoint
             if((t_elapsed > 0.75*t_line) && (t_elapsed < 1.25*t_line))
             {
                 //  Crossing: left(6), right(3) or straight ahead (0), for the corner straight does not exist
+                //Go to next place in route
             }
             else if ((t_elapsed > 0.15*t_line) && (t_elapsed < 0.65*t_line))
             {
                 //endpoint:Take a turn around left (00001111=15) or right (00001010=10)
+                //Go to next place in route
             }
-            //Go to next place in route
+            else signal_out = 0;
             break;
         case 10: //"00001010" ->Receiving error
             break; //Signal_out is not changed
