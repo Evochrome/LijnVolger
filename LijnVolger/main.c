df4f07b8c0b10f574eca844470f98c01e1478cc3
GitGhillie <jojo_jillis@hotmail.com>
2017-06-09 15:23:42 +0200

    xbee help

--- LijnVolger/main.c
+++ LijnVolger/main.c
@@ -6,10 +6,13 @@
 #include "typedefs.h"
 #include "maze.c"
 #include "router.c"
-#include "xbee.c"
+#include "maze.h"
+#include "router.h"
+#include "xbee.h"
 
 
 coords getCoords(char name[]);
+int decide_instruction(int signal_in);
 void init_time();
 double get_time();
 cell maze[13][13];
@@ -22,6 +25,7 @@ clock_t t_start;
 int main()
 {
     int READ;
+    int WRITE;
     byteBuffer[0] = '0';
     //Initialization of maze.
     initMinOnes(); //Generate grid of -1's
@@ -48,17 +52,22 @@ int main()
     //////////////////////////////
     //    Decide what do to based on byteBuffer here, and write to it.
 
-    gets(byteBuffer); //Temporary user input.
+    WRITE = decide_instruction(READ);
+
+
     //////////////////////////////
 
-
+    byteBuffer[0] = WRITE;
 
     writeByte(hSerial, byteBuffer);
 
-    if (byteBuffer[0] == 'q') // end the loop by typing 'q'
+    scanf("%d", &READ); //Temporary user input.
+
+    if (READ == 113) // end the loop by typing 'q'
         programStatus = 0;
 
-    displayMaze();
+
+    //displayMaze();
     }
 
 
@@ -96,5 +105,46 @@ void init_time()
 
 double get_time()
 {
-    return (double)((clock() - t_start)/CLOCKS_PER_SEC);
+    return ((double) (clock() - t_start) / CLOCKS_PER_SEC);
+}
+
+int decide_instruction(int signal_in)
+{
+    int signal_out;
+
+    double t_elapsed = get_time();
+    switch(signal_in)
+    {
+        case 9: //"00001001" -> mine
+            if((t_elapsed > 0.25*t_line) && (t_elapsed < 0.75*t_line))
+            {
+                //mine:Recheck route
+                //Take a turn around left (00001111=15) or right (00001010=10)
+                //Go to next place in route
+            }
+            else signal_out = 0;
+            break;
+
+        case 6: //"00000110" -> crossing, corner state, endpoint
+            printf("CASE 6\n");
+            if((t_elapsed > 0.75*t_line) && (t_elapsed < 1.25*t_line))
+            {
+                //  Crossing: left(6), right(3) or straight ahead (0), for the corner straight does not exist
+                //Go to next place in route
+            }
+            else if ((t_elapsed > 0.15*t_line) && (t_elapsed < 0.65*t_line))
+            {
+                //endpoint:Take a turn around left (00001111=15) or right (00001010=10)
+                //Go to next place in route
+            }
+            else signal_out = 0;
+            break;
+
+        case 10: //"00001010" ->Receiving error
+            break; //Signal_out is not changed
+
+        default: signal_out = 0; break;
+    }
+
+    return signal_out;
 }
